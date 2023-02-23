99f094b450e09cef671bd9e1e4e98b9ba2ea46c6
FERNANDO <fernandomacotto89@gmail.com>
2023-02-22 21:43:47 -0600

    Regreso v1 de correo

--- Views/Template/Email/email_cambioPassword.php
+++ Views/Template/Email/email_cambioPassword.php
@@ -91,86 +91,17 @@
 			<tr>
 				<td>
 					<div class="x_sgwrap">
-						<p>RECUPERACION <?= $data['nombreUsuario']; ?></p>
+						<p>Hola <?= $data['nombreUsuario']; ?></p>
 					</div>
-					<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
-					<tr>
-					<td align="center" style="padding:0">
-					<table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
-					<tr>
-					<tr>
-					<td style="padding:36px 30px 42px 30px;">
-					<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-					<tr>
-					<td style="padding:0 0 36px 0;color:#153643;">
-					<h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">INVERSIONES DEL ATLANTICO</h1>
-					<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"></p>;
-					<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://localhost/Inversiones_Atlantico_2/login" style="color:#ee4c50;text-decoration:underline;">INVERISONES DEL ATLANTICO</a></p>
-					</td>
-					</tr>
-					<tr>
-					<td style="padding:0;">
-					<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-					<tr>
-					<td style="width:260px;padding:0;vertical-align:top;color:#153643;">
-					<p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/left.gif" alt="" width="260" style="height:auto;display:block;" /></p>
-					<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">
-					<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://localhost/Inversiones_Atlantico_2/login" style="color:#ee4c50;text-decoration:underline;">Entrar</a></p>
-					</td>
-					<td style="width:20px;padding:0;font-size:0;line-height:0;">&nbsp;</td>;
-					<td style="width:260px;padding:0;vertical-align:top;color:#153643;">;
-					<p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/right.gif" alt="" width="260" style="height:auto;display:block;" /></p>
-					<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Contraseña con un tiempo de validez determinado por Inversiones del Atlantico.</p>
-					<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://localhost/Inversiones_Atlantico/php/index.php" style="color:#ee4c50;text-decoration:underline;">Entrar</a></p>
-					</td>
-					</tr>
-					</table>
-					</td>
-					</tr>
-					</table>
-					</td>
-					</tr>
-					<tr>
-					<td style="padding:30px;background:#1f9dc4;">
-					<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
-					<tr>
-					<td style="padding:0;width:50%;" align="left">
-					<p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">;
-					&reg; Diseños Alpha, Inventarios del Atlantico 2022<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">Unsubscribe</a>
-					</p>
-					</td>
-					<td style="padding:0;width:50%;" align="right">
-					<table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
-					<tr>
-					<td style="padding:0 0 0 10px;width:38px;">
-					<a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
-					</td>
-					<td style="padding:0 0 0 10px;width:38px;">;
-					<a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
-					</td>
-					</tr>
-					</table>
-					</td>
-					</tr>
-					</table>
-					</td>
-					</tr>
-					</table>
-					</td>
-					</tr>
-					</table>
-
-
-
-					<!-- <p>Solicitud de recuperacion para el usuario: <strong><?= $data['email']; ?></strong></p>
-					<p>Accede al enlace de abajo para confirmar. </p>
+					<p>Solicitud de acceso para el usuario: <strong><?= $data['email']; ?></strong></p>
+					<p>Has solicitado los datos de tu usuario, accede al enlace de abajo para confirmar tu contraseña. </p>
 					<p class="x_text_white">
-					<a href="<?= $data['url_recovery']; ?>" target="_blank" class="x_button_link x_link_blue">RECUPERAR</a>
+					<a href="<?= $data['url_recovery']; ?>" target="_blank" class="x_button_link x_link_blue">Confirmar datos</a>
 					</p>
 					<br>
 					<p>Si no te funciona el botón puedes copiar y pegar la siguiente dirección en tu navegador.</p>
 					<span><?= $data['url_recovery']; ?></span>
-					<p class="x_title_gray"><a href="<?= BASE_URL; ?>" target="_blanck"><?= WEB_EMPRESA; ?></a></p> -->
+					<p class="x_title_gray"><a href="<?= BASE_URL; ?>" target="_blanck"><?= WEB_EMPRESA; ?></a></p>
 				</td>
 			</tr>
 		</tbody>
