e805f407985823669f395b6de9b50a362b6441be
ElvinH5 <elvinh5@hotmail.com>
2023-03-10 02:13:27 -0600

    Centrar Modal de parámetro

--- Views/Template/Modals/modalParametros.php
+++ Views/Template/Modals/modalParametros.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="modalFormParametros" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-lg modal-dialog-centered" style="max-width: 500px;">
     <div class="modal-content">
       <div class="modal-header headerRegister">
         <h5 class="modal-title" id="titleModal">Nuevo Parámetro</h5>
@@ -15,14 +15,14 @@
           <input type="hidden" id="id_parametro" name="id_parametro" value="">
           <!--<p class="text-primary">Todos los campos son obligatorios.</p>-->
           <div class="form-row">
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-7">
               <label for="txtparametro">Parámetro</label>
               <input type="text" pattern="[a-zA-Z0-9_]+" class="form-control" id="txtparametro" name="txtparametro" style="text-transform:uppercase" required="" title="Ingrese solo letras y números sin espacios">
             </div>
           </div>
           <div class="form-row">
 
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-7">
               <label for="txtparametro">Valor</label>
               <input type="text" pattern="[a-zA-Z0-9_]+" class="form-control" id="txtvalor" name="txtvalor" style="text-transform:uppercase" required="" title="Ingrese solo letras y números sin espacios">
             </div>
