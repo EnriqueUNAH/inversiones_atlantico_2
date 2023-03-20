30dd228cd8dd86d7a5a4d8b27fe45919f0f9314d
DorcaAguilar <dorca.aguilar@unah.hn>
2023-03-19 23:46:06 -0600

    cerrar sesion bitacora

--- Models/LogoutModel.php
+++ Models/LogoutModel.php
@@ -12,22 +12,45 @@ class LogoutModel extends Mysql
 	}
 
 
+//Función para que inserte en bitácora cada vez que un usuario cierre sesion
+public function insertSalirBitacora(string $fecha, int $idUsuario, int $idObjeto, string $accion, string $descripcion)
+{
+	$this->dateFecha = $fecha;
+	$this->intIdUsuario = $idUsuario;
+	$this->intIdObjeto = $idObjeto;
+	$this->strAccion = $accion;
+	$this->strDescripcion = $descripcion;
+	$return = 0;
+
+	$query_insert  = "INSERT INTO tbl_ms_bitacora(fecha,id_usuario,id_objeto,accion,descripcion) 
+							  VALUES(?,?,?,?,?)";
+	$arrData = array(
+		$this->dateFecha,
+		$this->intIdUsuario,
+		$this->intIdObjeto,
+		$this->strAccion,
+		$this->strDescripcion
+	);
+	$request_insert = $this->insert($query_insert, $arrData);
+	$return = $request_insert;
+
+	return $return;
+}
 
 
+	// public function updateUltimaConexion(int $idUsuario)
+	// {
+	// 	$this->intIdUsuario = $idUsuario;
+	// 	$this->dateFecha = date('Y-m-d H:i:s');
 
-	public function updateUltimaConexion(int $idUsuario)
-	{
-		$this->intIdUsuario = $idUsuario;
-		$this->dateFecha = date('Y-m-d H:i:s');
+	// 	$sql = "UPDATE tbl_ms_usuarios SET fecha_ultima_conexion=? WHERE id_usuario=?";
 
-		$sql = "UPDATE tbl_ms_usuarios SET fecha_ultima_conexion=? WHERE id_usuario=?";
+	// 	$arrData = array(
+	// 		$this->dateFecha,
+	// 		$this->intIdUsuario
+	// 	);
 
-		$arrData = array(
-			$this->dateFecha,
-			$this->intIdUsuario
-		);
-
-		$request = $this->update($sql, $arrData);
-		return $request;
-	}
+	// 	$request = $this->update($sql, $arrData);
+	// 	return $request;
+	// }
 }
