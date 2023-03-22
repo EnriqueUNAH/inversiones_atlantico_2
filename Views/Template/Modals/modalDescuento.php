e7a479ddaf57194b7872d38926794038fbea6882
FERNANDO <fernandomacotto89@gmail.com>
2023-03-21 20:15:38 -0600

    descuento

--- Views/Template/Modals/modalDescuento.php
+++ Views/Template/Modals/modalDescuento.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
-<div class="modal fade" id="modalFormDescuento" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg modal-dialog-centered" style="max-width: 500px;">
+<div class="modal fade" id="modalFormDescuentos" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-xl" >
     <div class="modal-content">
       <div class="modal-header headerRegister">
         <h5 class="modal-title" id="titleModal">Nuevo Descuento</h5>
@@ -9,79 +9,68 @@
         </button>
       </div>
       <div class="modal-body">
-
-
-        <form id="formDescuento" name="formDescuento" class="form-horizontal">
-          <input type="hidden" id="id_parametro" name="id_parametro" value="">
-          <!--<p class="text-primary">Todos los campos son obligatorios.</p>-->
-          <div class="form-row">
-            <div class="form-group col-md-7">
-              <label for="txtdescuento">Nombre Descuento</label>
-              <input type="text" pattern="[a-zA-Z0-9_]+" class="form-control" id="txtdescuento" name="txtdescuetno" style="text-transform:uppercase" required="" title="Ingrese solo letras y números sin espacios">
-            </div>
-          </div>
-          <div class="form-row">
-
-            <div class="form-group col-md-7">
-              <label for="txtparametro">Valor</label>
-              <input type="text" pattern="[a-zA-Z0-9_]+" class="form-control" id="txtvalor" name="txtvalor" style="text-transform:uppercase" required="" title="Ingrese solo letras y números sin espacios">
-            </div>
-
-          </div>
-
-
-          <div class="tile-footer">
-            <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
-            <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
-          </div>
-        </form>
-
+            <form id="formDescuentos" name="formDescuentos" class="form-horizontal">
+              <input type="hidden" id="cod_descuento" name="cod_descuento" value="">
+              <p class="text-primary">Los campos con asterisco (<span class="required">*</span>) son obligatorios.</p>
+              <div class="row">
+                <div class="col-md-8">
+                    <div class="form-group">
+                      <label class="control-label">Nombre Descuento <span class="required">*</span></label>
+                      <input class="form-control" id="txtNombre" name="txtNombre" type="text" required="">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label class="control-label">Porcentaje descuento <span class="required">*</span></label>
+                            <input class="form-control" id="txtPorcentaje" name="txtPorcentaje" type="text" required="">
+                        </div>
+                    </div>
+                    <div class="row">
+                       <div class="form-group col-md-6">
+                           <button id="btnActionForm" class="btn btn-primary btn-lg btn-block" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>
+                       </div> 
+                       <div class="form-group col-md-6">
+                           <button class="btn btn-danger btn-lg btn-block" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
+                       </div> 
+                    </div>  
+                </div>
+              </div>    
+                     </div>
+                 </div>
+                                
+              </div>
+            </form>
       </div>
     </div>
   </div>
 </div>
 
-
-
-<!-- Modal del botón VER MÁS-->
-<div class="modal fade" id="modalViewDescuento" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog">
+<!-- Modal -->
+<div class="modal fade" id="modalViewDescuentos" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-xl" >
     <div class="modal-content">
       <div class="modal-header header-primary">
-        <h5 class="modal-title" id="titleModal">Datos de los parámetros</h5>
+        <h5 class="modal-title" id="titleModal">Editar Descuento</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-      </div>
+      </div>odal
       <div class="modal-body">
         <table class="table table-bordered">
           <tbody>
             <tr>
-              <td>Parámetro:</td>
-              <td id="elParametro"></td>
+              <td>Codigo:</td>
+              <td id="celCodigo"></td>
             </tr>
             <tr>
-              <td>Valor:</td>
-              <td id="elValor"></td>
-            </tr>
-
-            <tr>
-              <td>Creado Por:</td>
-              <td id="elCreadoPor"></td>
+              <td>Nombres:</td>
+              <td id="celNombre"></td>
             </tr>
             <tr>
-              <td>Fecha Creación:</td>
-              <td id="elFechaCreacion"></td>
+              <td>Porcentaje:</td>
+              <td id="celPorcentaje"></td>
             </tr>
-            <tr>
-              <td>Modificado Por:</td>
-              <td id="elModificadoPor"></td>
-            </tr>
-            <tr>
-              <td>Fecha Modificación:</td>
-              <td id="elFechaModificacion"></td>
-            </tr>
-
           </tbody>
         </table>
       </div>
