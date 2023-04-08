8de556445ec6520927ffc89d8f72d72f13eac11f
ElvinH5 <elvinh5@hotmail.com>
2023-04-07 23:54:16 -0600

    Cosas que faltaban en la tabla Descuento.

--- Assets/js/functions_descuentos.js
+++ Assets/js/functions_descuentos.js
@@ -1,195 +1,213 @@
-let tableDescuentos; 
+let tableDescuentos;
 let rowTable = "";
 let divLoading = document.querySelector("#divLoading");
-document.addEventListener('DOMContentLoaded', function(){
-
-    tableDescuentos = $('#tableDescuentos').dataTable( {
-        "aProcessing":true,
-        "aServerSide":true,
-        "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
-        },
-        "ajax":{
-            "url": " "+base_url+"/Descuentos/getDescuentos",
-            "dataSrc":""
-        },
-        "columns":[
-            {"data":"cod_descuento"},
-            {"data":"nombre_descuento"},
-            {"data":"porcentaje_descuento"},
-            {"data":"options"}
-        ],
-        'dom': 'lBfrtip',
-        'buttons': [
-            {
-                "extend": "excelHtml5",
-                "text": "<i class='fas fa-file-excel'></i> Excel",
-                "titleAttr":"Esportar a Excel",
-                "className": "btn btn-success"
-            },{
-                "extend": "pdfHtml5",
-                "text": "<i class='fas fa-file-pdf'></i> PDF",
-                "titleAttr":"Esportar a PDF",
-                "className": "btn btn-danger"
-            }
-        ],
-        "resonsieve":"true",
-        "bDestroy": true,
-        "iDisplayLength": 10,
-        "order":[[0,"desc"]]  
-    });
-
-	if(document.querySelector("#formDescuento")){
-        let formDescuento = document.querySelector("#formDescuento");
-        formDescuento.onsubmit = function(e) {
-            e.preventDefault();
-            let intIdCodigo = document.querySelector('#txtIdCodigo').value;
-            let strNombre = document.querySelector('#txtNombre').value;
-            let intPorcentaje = document.querySelector('#txtPorcentaje').value;
-
-            if(strNombre == '' || intPorcentaje == '')
-            {
-                swal("Atención", "Todos los campos son obligatorios." , "error");
-                return false;
-            }
-
-            let elementsValid = document.getElementsByClassName("valid");
-            for (let i = 0; i < elementsValid.length; i++) { 
-                if(elementsValid[i].classList.contains('is-invalid')) { 
-                    swal("Atención", "Por favor verifique los campos en rojo." , "error");
-                    return false;
-                } 
-            } 
-            divLoading.style.display = "flex";
-            let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-            let ajaxUrl = base_url+'/Descuentos/setDescuento'; 
-            let formData = new FormData(formDescuento);
-            request.open("POST",ajaxUrl,true);
-            request.send(formData);
-            request.onreadystatechange = function(){
-                if(request.readyState == 4 && request.status == 200){
-                    let objData = JSON.parse(request.responseText);
-                    if(objData.status)
-                    {
-                        if(rowTable == ""){
-                            tableDescuentos.api().ajax.reload();
-                        }else{
-                           rowTable.cells[0].textContent =  cod_descuento;
-                           rowTable.cells[1].textContent =  strNombre;
-                           rowTable.cells[2].textContent =  intPorcentaje;
-                           rowTable = "";
-                        }
-                        $('#modalFormDescuentos').modal("hide");
-                        formDescuento.reset();
-                        swal("Descuento", objData.msg ,"success");
-                    }else{
-                        swal("Error", objData.msg , "error");
-                    }
-                }
-                divLoading.style.display = "none";
-                return false;
-            }
-        }
-    }
-
-
-}, false);
-
-
-function fntViewInfo(cod_descuento){
-    let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    let ajaxUrl = base_url+'/Descuentos/getDescuento/'+cod_descuento;
-    request.open("GET",ajaxUrl,true);
-    request.send();
-    request.onreadystatechange = function(){
-        if(request.readyState == 4 && request.status == 200){
-            let objData = JSON.parse(request.responseText);
-            if(objData.status)
-            {
-                document.querySelector("#txtIdCodigo").innerHTML = objData.data.cod_descuento;
-                document.querySelector("#txtNombre").innerHTML = objData.data.nombre;
-                document.querySelector("#txtPorcentaje").innerHTML = objData.data.porcentaje;
-                $('#modalViewDescuento').modal('show');
-            }else{
-                swal("Error", objData.msg , "error");
-            }
-        }
-    }
-}
-
-function fntEditInfo(element, cod_descuento){
-    rowTable = element.parentNode.parentNode.parentNode;
-    document.querySelector('#titleModal').innerHTML ="Actualizar Descuento";
-    document.querySelector('.modal-header').classList.replace("headerRegister", "headerUpdate");
-    document.querySelector('#btnActionForm').classList.replace("btn-primary", "btn-info");
-    document.querySelector('#btnText').innerHTML ="Actualizar";
-    let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    let ajaxUrl = base_url+'/Descuentos/getDescuento/'+cod_descuento;
-    request.open("GET",ajaxUrl,true);
-    request.send();
-    request.onreadystatechange = function(){
-
-        if(request.readyState == 4 && request.status == 200){
-            let objData = JSON.parse(request.responseText);
-            if(objData.status)
-            {
-                // document.querySelector("#idUsuario").value = objData.data.idpersona;
-                document.querySelector("#txtIdCodigo").value = objData.data.cod_descuento;
-                document.querySelector("#txtNombre").value = objData.data.nombre_descuento;
-                document.querySelector("#txtPorcentaje").value = objData.data.porcentaje_descuento;
-            }
-        }
-        $('#modalFormDescuento').modal('show');
-    }
-}
-
-
-function fntDelInfo(cod_descuento){
-    swal({
-        title: "Eliminar Descuento",
-        text: "¿Realmente quiere eliminar el descuento?",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonText: "Si, eliminar!",
-        cancelButtonText: "No, cancelar!",
-        closeOnConfirm: false,
-        closeOnCancel: true
-    }, function(isConfirm) {
-        
-        if (isConfirm) 
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    tableDescuentos = $("#tableDescuentos").dataTable({
+      aProcessing: true,
+      aServerSide: true,
+      language: {
+        url: "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json",
+      },
+      ajax: {
+        url: " " + base_url + "/Descuentos/getDescuentos",
+        dataSrc: "",
+      },
+      columns: [
+        { data: "nombre_descuento" },
+        { data: "porcentaje_descuento" },
+        { data: "options" },
+      ],
+      dom: "lBfrtip",
+      buttons: [
         {
-            let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-            let ajaxUrl = base_url+'/Descuentos/delDescuento';
-            let strData = "cod_descuento="+cod_descuento;
-            request.open("POST",ajaxUrl,true);
-            request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            request.send(strData);
-            request.onreadystatechange = function(){
-                if(request.readyState == 4 && request.status == 200){
-                    let objData = JSON.parse(request.responseText);
-                    if(objData.status)
-                    {
-                      swal("Atención!", objData.msg , "error");
-                    }else{
-                        swal("Eliminar!", objData.msg , "success");
-                        tableDescuentos.api().ajax.reload();
-                    }
-                }
-            }
-        }
-  
+          extend: "excelHtml5",
+          text: "<i class='fas fa-file-excel'></i> Excel",
+          titleAttr: "Esportar a Excel",
+          className: "btn btn-success",
+        },
+        {
+          extend: "pdfHtml5",
+          text: "<i class='fas fa-file-pdf'></i> PDF",
+          titleAttr: "Esportar a PDF",
+          className: "btn btn-danger",
+        },
+      ],
+      resonsieve: "true",
+      bDestroy: true,
+      iDisplayLength: 10,
+      order: [[0, "desc"]],
     });
-  
-  }
 
-function openModal()
-{
-    rowTable = "";
-    // document.querySelector('#idUsuario').value ="";
-    document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
-    document.querySelector('#btnActionForm').classList.replace("btn-info", "btn-primary");
-    document.querySelector('#btnText').innerHTML ="Guardar";
-    document.querySelector('#titleModal').innerHTML = "Nuevo Descuento";
-    document.querySelector("#formDescuento").reset();
-    $('#modalFormDescuento').modal('show');
+    if (document.querySelector("#formDescuento")) {
+      let formDescuento = document.querySelector("#formDescuento");
+      formDescuento.onsubmit = function (e) {
+        e.preventDefault();
+        let intIdCodigo = document.querySelector("#txtIdCodigo").value;
+        let strNombre = document.querySelector("#txtNombre").value;
+        let intPorcentaje = document.querySelector("#txtPorcentaje").value;
+
+        if (strNombre == "" || intPorcentaje == "") {
+          swal("Atención", "Todos los campos son obligatorios.", "error");
+          return false;
+        }
+
+        let elementsValid = document.getElementsByClassName("valid");
+        for (let i = 0; i < elementsValid.length; i++) {
+          if (elementsValid[i].classList.contains("is-invalid")) {
+            swal(
+              "Atención",
+              "Por favor verifique los campos en rojo.",
+              "error"
+            );
+            return false;
+          }
+        }
+        divLoading.style.display = "flex";
+        let request = window.XMLHttpRequest
+          ? new XMLHttpRequest()
+          : new ActiveXObject("Microsoft.XMLHTTP");
+        let ajaxUrl = base_url + "/Descuentos/setDescuento";
+        let formData = new FormData(formDescuento);
+        request.open("POST", ajaxUrl, true);
+        request.send(formData);
+        request.onreadystatechange = function () {
+          if (request.readyState == 4 && request.status == 200) {
+            let objData = JSON.parse(request.responseText);
+            if (objData.status) {
+              if (rowTable == "") {
+                tableDescuentos.api().ajax.reload();
+              } else {
+                rowTable.cells[0].textContent = strNombre;
+                rowTable.cells[1].textContent = intPorcentaje;
+                rowTable = "";
+              }
+              $("#modalFormDescuentos").modal("hide");
+              formDescuento.reset();
+              swal("Descuento", objData.msg, "success");
+            } else {
+              swal("Error", objData.msg, "error");
+            }
+          }
+          divLoading.style.display = "none";
+          return false;
+        };
+      };
+    }
+  },
+  false
+);
+
+function fntViewInfo(cod_descuento) {
+  let request = window.XMLHttpRequest
+    ? new XMLHttpRequest()
+    : new ActiveXObject("Microsoft.XMLHTTP");
+  let ajaxUrl = base_url + "/Descuentos/getDescuento/" + cod_descuento;
+  request.open("GET", ajaxUrl, true);
+  request.send();
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      let objData = JSON.parse(request.responseText);
+      if (objData.status) {
+        document.querySelector("#txtIdCodigo").innerHTML =
+          objData.data.cod_descuento;
+        document.querySelector("#txtNombre").innerHTML = objData.data.nombre;
+        document.querySelector("#txtPorcentaje").innerHTML =
+          objData.data.porcentaje;
+        $("#modalViewDescuento").modal("show");
+      } else {
+        swal("Error", objData.msg, "error");
+      }
+    }
+  };
+}
+
+function fntEditInfo(element, cod_descuento) {
+  rowTable = element.parentNode.parentNode.parentNode;
+  document.querySelector("#titleModal").innerHTML = "Actualizar Descuento";
+  document
+    .querySelector(".modal-header")
+    .classList.replace("headerRegister", "headerUpdate");
+  document
+    .querySelector("#btnActionForm")
+    .classList.replace("btn-primary", "btn-info");
+  document.querySelector("#btnText").innerHTML = "Actualizar";
+  let request = window.XMLHttpRequest
+    ? new XMLHttpRequest()
+    : new ActiveXObject("Microsoft.XMLHTTP");
+  let ajaxUrl = base_url + "/Descuentos/getDescuento/" + cod_descuento;
+  request.open("GET", ajaxUrl, true);
+  request.send();
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      let objData = JSON.parse(request.responseText);
+      if (objData.status) {
+        // document.querySelector("#idUsuario").value = objData.data.idpersona;
+        document.querySelector("#txtIdCodigo").value =
+          objData.data.cod_descuento;
+        document.querySelector("#txtNombre").value =
+          objData.data.nombre_descuento;
+        document.querySelector("#txtPorcentaje").value =
+          objData.data.porcentaje_descuento;
+      }
+    }
+    $("#modalFormDescuentos").modal("show");
+  };
+}
+
+function fntDelInfo(cod_descuento) {
+  swal(
+    {
+      title: "Eliminar Descuento",
+      text: "¿Realmente quiere eliminar el descuento?",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Si, eliminar!",
+      cancelButtonText: "No, cancelar!",
+      closeOnConfirm: false,
+      closeOnCancel: true,
+    },
+    function (isConfirm) {
+      if (isConfirm) {
+        let request = window.XMLHttpRequest
+          ? new XMLHttpRequest()
+          : new ActiveXObject("Microsoft.XMLHTTP");
+        let ajaxUrl = base_url + "/Descuentos/delDescuento";
+        let strData = "cod_descuento=" + cod_descuento;
+        request.open("POST", ajaxUrl, true);
+        request.setRequestHeader(
+          "Content-type",
+          "application/x-www-form-urlencoded"
+        );
+        request.send(strData);
+        request.onreadystatechange = function () {
+          if (request.readyState == 4 && request.status == 200) {
+            let objData = JSON.parse(request.responseText);
+            if (objData.status) {
+              swal("Atención!", objData.msg, "error");
+            } else {
+              swal("Eliminar!", objData.msg, "success");
+              tableDescuentos.api().ajax.reload();
+            }
+          }
+        };
+      }
+    }
+  );
+}
+
+function openModal() {
+  rowTable = "";
+  // document.querySelector('#idUsuario').value ="";
+  document
+    .querySelector(".modal-header")
+    .classList.replace("headerUpdate", "headerRegister");
+  document
+    .querySelector("#btnActionForm")
+    .classList.replace("btn-info", "btn-primary");
+  document.querySelector("#btnText").innerHTML = "Guardar";
+  document.querySelector("#titleModal").innerHTML = "Nuevo Descuento";
+  document.querySelector("#formDescuento").reset();
+  $("#modalFormDescuentos").modal("show");
 }
--- Views/Template/Modals/modalDescuento.php
+++ Views/Template/Modals/modalDescuento.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
-<div class="modal fade" id="modalFormDescuento" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg" >
+<div class="modal fade" id="modalFormDescuentos" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header headerRegister">
         <h5 class="modal-title" id="titleModal">Nuevo Descuento</h5>
@@ -9,52 +9,52 @@
         </button>
       </div>
       <div class="modal-body">
-            <form id="formDescuento" name="formDescuento" class="form-horizontal">
-              <input type="hidden" id="idUsuario" name="idUsuario" value="">
-              <!-- <p class="text-primary">Los campos con asterisco (<span class="required">*</span>) son obligatorios.</p> -->
+        <form id="formDescuento" name="formDescuento" class="form-horizontal">
+          <input type="hidden" id="idUsuario" name="idUsuario" value="">
+          <!-- <p class="text-primary">Los campos con asterisco (<span class="required">*</span>) son obligatorios.</p> -->
 
-              <div class="form-row">
-                <div>
-                  <label type="hidden" for="txtIdCodigo"></label>
-                  <input type="hidden" class="form-control" id="txtIdCodigo" name="txtIdCodigo">
-                </div>
-                <div class="form-group col-md-4">
-                  <label for="txtNombre">Nombre Descuento<span class="required">*</span></label>
-                  <input type="text" onkeyup="this.value = this.value.toUpperCase();" class="form-control valid validText" id="txtNombre" name="txtNombre" required="">
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-4">
-                  <label for="txtPorcentaje">Porcentaje <span class="required">*</span></label>
-                  <input type="number" step="0.01" class="form-control" id="txtPorcentaje" name="txtPorcentaje" required="">
-                </div>
-              </div>
-              <hr>
-              <!-- <p class="text-primary">Datos Fiscales.</p>-->
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <!-- <label>Identificación Tributaria <span class="required">*</span></label>
+          <div class="form-row">
+            <div>
+              <label type="hidden" for="txtIdCodigo"></label>
+              <input type="hidden" class="form-control" id="txtIdCodigo" name="txtIdCodigo">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="txtNombre">Nombre Descuento<span class="required">*</span></label>
+              <input type="text" onkeyup="this.value = this.value.toUpperCase();" class="form-control valid validText" id="txtNombre" name="txtNombre" required="">
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="txtPorcentaje">Porcentaje <span class="required">*</span></label>
+              <input type="number" step="0.01" class="form-control" id="txtPorcentaje" name="txtPorcentaje" required="">
+            </div>
+          </div>
+          <hr>
+          <!-- <p class="text-primary">Datos Fiscales.</p>-->
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <!-- <label>Identificación Tributaria <span class="required">*</span></label>
                   <input class="form-control" type="text" id="txtNit" name="txtNit" required=""> -->
-                </div> 
-                <div class="form-group col-md-6">
-                  <!-- <label>Nombre fiscal <span class="required">*</span></label>
+            </div>
+            <div class="form-group col-md-6">
+              <!-- <label>Nombre fiscal <span class="required">*</span></label>
                   <input class="form-control" type="text" id="txtNombreFiscal" name="txtNombreFiscal" required=""> -->
-                </div> 
+            </div>
 
-                <div class="form-group col-md-12">
-                  <!-- <label>Dirección fiscal <span class="required">*</span></label>
+            <div class="form-group col-md-12">
+              <!-- <label>Dirección fiscal <span class="required">*</span></label>
                   <input class="form-control" type="text" id="txtDirFiscal" name="txtDirFiscal" required=""> -->
-                </div> 
-                
-              </div>
-             <div class="form-row">
-                
-             </div>
-              <div class="tile-footer">
-                <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
-                <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
-              </div>
-            </form>
+            </div>
+
+          </div>
+          <div class="form-row">
+
+          </div>
+          <div class="tile-footer">
+            <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
+            <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cerrar</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -62,7 +62,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modalViewDescuento" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog" >
+  <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header header-primary">
         <h5 class="modal-title" id="titleModal">Datos del Descuento</h5>
@@ -78,12 +78,12 @@
               <td ></td> -->
             </tr>
             <tr>
-              <td id="Nombre" >Nombre descuento:</td>
-              <td ></td>
+              <td id="Nombre">Nombre descuento:</td>
+              <td></td>
             </tr>
             <tr>
               <td id="Porcentaje">Porcentaje:</td>
-              <td ></td>
+              <td></td>
             </tr>
           </tbody>
         </table>
@@ -94,4 +94,3 @@
     </div>
   </div>
 </div>
-
