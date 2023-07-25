decb670b5be95129beb80701424b95be8f89b481
Elvin Hernández <94201127+ElvinAlejandro@users.noreply.github.com>
2023-07-25 02:14:47 -0600

    Alerta, preguntas contestadas correctamente.

--- Views/Login/preguntas.php
+++ Views/Login/preguntas.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
+	
+    <!-- Para las alertas -->
+	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
+	    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
+    <!-- Para las alertas -->
+    
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+
+</head>
+
+<body></body>
+
+</html>
 <?php
 include('db.php');
 $respuesta = strtoupper(($_POST['respuesta']));
@@ -87,6 +108,7 @@ if ($valor_contestadas < $valor_p_p - 1) { ////////////////////////////////////C
     $actualizarPre = "UPDATE tbl_ms_usuarios SET preguntas_contestadas = '$contestadas' where id_usuario = '$id_Usuario'";
     mysqli_query($conexion, $actualizarPre);
 
+ 
 
     echo '<script>alert("Respuesta Guardada");</script>';
     include("../Login/preguntasPrimeraVez.php");
@@ -103,10 +125,25 @@ if ($valor_contestadas < $valor_p_p - 1) { ////////////////////////////////////C
     mysqli_close($conexion);
 
 ?>
-    <script>
+    <!-- <script>
         alert("Preguntas Contestadas Correctamente");
         location.href = "cambiar_contrasena_recu.php";
-    </SCRipt><?php
+    </SCRipt> -->
+    
+    <script>
+                    Swal.fire({
+              title: "Preguntas Contestadas Correctamente",
+              icon: "success",
+              confirmButtonText: "OK",
+            }).then(() => {
+                location.href = "cambiar_contrasena_recu.php";
+            });
+        
+    </SCRipt>
+    
+    
+    
+    <?php
             }
 
 
